33cd5749d0560df789d39b19bc4aa31358438d8a
bryanhighhill <bhighhill@basis.cloud>
2024-02-12 10:05:56 -0700

    js function is adding css class and sliding the right column over/making left column 10% width

--- Collapsable_Menu_Test/public/script.js
+++ Collapsable_Menu_Test/public/script.js
@@ -1,18 +1,14 @@
 /**
  * 
  */
-let menuCollapsed = false;
  
+let menuCollapsed = "false";
+
 function handleButtonClick() {
-	let leftColumn = document.getElementById("1");
-	 
-	if (menuCollapsed = true) {
-	    leftColumn.style.display = "flex";
-		console.log(`you opened the menu with: ${menuCollapsed}`);
-		menuCollapsed = true;
-	} else {
-	    leftColumn.style.display = "none";
-		console.log(`you opened the menu with: ${menuCollapsed}`);
-		menuCollapsed = false;
-	}
- }
+	const leftColumn = document.querySelector(".column-left");
+	const rightColumn = document.querySelector(".column-right");
+	
+	console.log(` left and right columns ${leftColumn} + ${rightColumn}`)
+	
+	leftColumn.classList.toggle('collapse-sidebar');	 
+}
