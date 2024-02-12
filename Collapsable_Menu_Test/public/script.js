3e5fbdf925704c48de2c6c8372410f67350110f2
bryanhighhill <bhighhill@basis.cloud>
2024-02-12 11:18:00 -0700

    removed unused js variables

--- Collapsable_Menu_Test/public/script.js
+++ Collapsable_Menu_Test/public/script.js
@@ -7,9 +7,7 @@ let menuCollapsed = "false";
 function handleButtonClick() {
 	const leftColumn = document.querySelector(".column-left");
 	const rightColumn = document.querySelector(".column-right");
-	const sideBarContent = document.querySelector(".sidebar-content");
 	
 	rightColumn.classList.toggle('enlarge-body');	 
 	leftColumn.classList.toggle('collapse-sidebar');
-	sideBarContent.classList.toggle('collapse-sidebar-content');
 }
